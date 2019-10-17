46298d7ba4c4a903a0e8e358d95aeca4785a7974
Tom <thoward12@outlook.com>
2019-10-17 08:41:45 -0400

    documentation

--- grade/mixins.py
+++ grade/mixins.py
@@ -5,25 +5,21 @@ from typing import List, Callable
 from collections import namedtuple
 
 class ScoringMixin:
+    """ Provides scoring utility functions.
+    """
 
     methods : List[tuple] = None
 
     def __str__(self) -> str:
         return type(self).__name__
 
-    @staticmethod
-    def istest(x) -> bool:
-        if ismethod(x):
-            breakpoint()
-        return ismethod(x) and x.__name__.startswith('test')
-
     def getTests(self) -> List[Callable]:
-        """ Returns a list of test methods. """
+        """ Returns a list of all test methods. """
         return [(key, getattr(self, key)) for key in dir(self) if key.startswith('test')]
 
 
     def getTest(self) -> Callable:
-        """ Returns the topmost testing function on the stack. """
+        """ Returns the topmost test method on the stack. """
         if not self.methods:
             self.methods = self.getTests()
         
@@ -33,35 +29,50 @@ class ScoringMixin:
 
     @property
     def weight(self) -> int:
+        """ Returns the weight of the test. """
         return getattr(self.getTest().__dict__, '__weight__', 0)
 
     @weight.setter
-    def weight(self, weight):
+    def weight(self, weight: int) -> None:
+        """ Sets the weight of the test. """
         self.getTest().__dict__['__weight__'] = weight
 
     @property
-    def score(self):
+    def score(self) -> Union[int, float]:
+        """ Returns the current score for the test. """
         return getattr(self.getTest().__dict__, '__score__', 0)
     
     @score.setter
-    def score(self, score):
+    def score(self, score: Union[int, float]) -> None:
+        """ Sets the score for the test. """
         self.getTest().__dict__['__score__'] = score
 
     @property
-    def visibility(self):
+    def visibility(self) -> str:
+        """ Returns visibility of the test. 
+        
+        This controls whether or not students should see the failing testcase.
+        """
         return getattr(self.getTest().__dict__['__visibility__'], 'visible')
 
-    def visibility(self, visibility):
+    @visibility.setter
+    def visibility(self, visibility: str) -> None:
+        """ Sets the visibility of the test. """
         self.getTest().__dict__['__visibility__'] = visibility
 
     @property
     def leaderboard(self) -> dict:
+        """ Returns a dictionary with all leaderboard attributes. """
         return {
             'title': getattr(self.getTest(), '__leaderboard_title__', None),
             'order': getattr(self.getTest(), '__leaderboard_order__', None),
             'score': getattr(self.getTest(), '__leaderboard_score__', None)
         }
 
-    def leaderboard(self, **kwargs):
+    def leaderboard(self, **kwargs) -> None:
+        """ Updates leaderboard attributes.
+        
+        All attributes must start with __leaderboard.
+        """
         assert(all([p.startswith('__leaderboard') for p in kwargs.keys()]))
         self.getTest().__dict__.update(**kwargs)
