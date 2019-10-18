77ec0c529a8aa91a823f1e005b09a48d2a37b1aa
Tom <thoward12@outlook.com>
2019-10-17 22:29:12 -0400

    Add setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='grade',
+    version='1.0',
+    packages=['grade'],
+    url='https://github.com/thoward27/grade',
+    license='AGPL',
+    author='Tom Howard',
+    author_email='info@tomhoward.codes',
+    description='An package for easy autograding.',
+    long_description=long_description,
+    python_requires='>=3.8',
+)
