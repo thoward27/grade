0a7feee2cf560d27f6f504ba291e4f9937e53cd5
Tom <thoward12@outlook.com>
2019-10-20 12:00:19 -0400

    Capturing output.

--- grade/result.py
+++ grade/result.py
@@ -1,4 +1,4 @@
-from typing import TextIO
+from typing import TextIO, List, Tuple
 from unittest import result
 import unittest
 import json
@@ -21,7 +21,7 @@ class Result(unittest.TextTestResult):
 
     @property
     def json(self) -> dict:
-        return self.data
+        return json.dumps(self.data, indent=4, sort_keys=True) 
 
     @property
     def markdown(self) -> str:
@@ -32,40 +32,56 @@ class Result(unittest.TextTestResult):
         return getattr(getattr(test, test._testMethodName), attribute, default)
 
     @staticmethod
-    def parseExceptions(failures, errors):
-        output = ''.join([*failures, *errors])
-        output = '; '.join([line.strip() for line in output.split('\n')[1:]])
+    def parseExceptions(exceptions):
+        output = '; '.join(exceptions)
+        output = '; '.join([line.strip() for line in output.split('\n')[1:] if line])
         # Shave output to: final python file, failures/errors
         output = ''.join([output.split('.py')[-2].split('/')[-1] + '.py', output.split('.py\"')[-1]])
         return output.strip()
 
-    def stopTest(self, test):
-        super().stopTest(test)
-        # Basic Information
-        result = {
-            'name': self.getattr(test, '__name__'),
-            'max_score': self.getattr(test, '__weight__', 0),
-            'score': self.getattr(test, '__score__', None),
-        }
+    def getExceptions(self, test) -> List[Tuple]:
+        return [m for f, m in [*self.failures, *self.errors] if f == test]
 
-        # Add to name.
+    def getName(self, test):
+        name = self.getattr(test, '__name__')
         if description := test.shortDescription():
-            result['name'] = result['name'] + ': ' + description
+            name = f'{name}: {description}'
+        return name
 
-        # Parse any exceptions out.
-        failures = [msg for func, msg in self.failures if func == test]
-        errors = [msg for func, msg in self.errors if func == test]
-        if failures or errors:
-            result['output'] = self.parseExceptions(failures, errors)
+    def getScore(self, test):
+        return self.getattr(test, '__score__', 0 if self.getExceptions(test) else self.getattr(test, '__weight__', 0))
 
-        # Update score if needed.
-        if result['score'] is None:
-            result['score'] = 0 if failures or errors else result['max_score']
+    def addError(self, test, err):
+        super().addError(test, err)
+        self._mirrorOutput = False
+
+    def addFailure(self, test, err):
+        super().addFailure(test, err)
+        self._mirrorOutput = False
+
+    def stopTest(self, test):
+        self.updateTests(test)
+        self.updateLeaderboard(test)
+        super().stopTest(test)
+        return
+    
+    def updateTests(self, test):
+        result = {
+            'name': self.getName(test),
+            'max_score': self.getattr(test, '__weight__', 0),
+            'score': self.getScore(test),
+        }
+        if exceptions := self.getExceptions(test):
+            result['output'] = self.parseExceptions(exceptions)
         
-        # Append the result.
+        elif outputs := (self._stdout_buffer.getvalue() + self._stderr_buffer.getvalue()).strip():
+            result['output'] = outputs
+        
         self.data['tests'].append(result)
-        
-        # Update leaderboard, if needed.
+        return
+
+
+    def updateLeaderboard(self, test):
         if self.getattr(test, '__leaderboard_title__') is not None:
             self.data['leaderboard'].append({
                 'name': self.getattr(test, '__leaderboard_title__'),
