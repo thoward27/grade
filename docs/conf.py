878e7f2d19110991883531462cf52c2c809d470f
Tom <thoward12@outlook.com>
2019-12-13 22:22:14 -0500

    Small tweaks to imports and Dockerfile.
    Signed-off-by: Tom <thoward12@outlook.com>

--- docs/conf.py
+++ docs/conf.py
@@ -12,9 +12,10 @@
 #
 import os
 import sys
+
 from pkg_resources import get_distribution
+
 sys.path.insert(0, os.path.abspath('..'))
-import grade
 
 
 # -- Project information -----------------------------------------------------
